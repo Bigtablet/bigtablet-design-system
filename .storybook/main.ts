08b303deba022edd03455e2d4c86c30e2cedbc95
상만 <a01071397193@gmail.com>
2025-11-05 15:37:08 +0900

    config: storybook scss compatibility error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,28 @@
+import path from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../stories/**/*.stories.@(ts|tsx)"],
     addons: ["@storybook/addon-essentials"],
-    framework: { name: "@storybook/react-vite", options: {} }
+    framework: { name: "@storybook/react-vite", options: {} },
+    viteFinal: async (cfg) => {
+        cfg.resolve = cfg.resolve || {};
+        cfg.resolve.alias = {
+            ...(cfg.resolve.alias || {}),
+            "src": path.resolve(__dirname, "../src"),
+        };
+        cfg.css = {
+            ...(cfg.css || {}),
+            preprocessorOptions: {
+                scss: {
+                    ...(cfg.css?.preprocessorOptions?.scss || {}),
+                    loadPaths: ["src"],
+                    quietDeps: true,
+                },
+            },
+        };
+        return cfg;
+    },
 };
 
 export default config;
