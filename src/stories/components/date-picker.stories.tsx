3ed3e8e884c2417f67c91bab2bf69cd6a5a4ab8d
상만 <a01071397193@gmail.com>
2026-01-13 10:27:39 +0900

    fix: leap year handling

--- src/stories/components/date-picker.stories.tsx
+++ src/stories/components/date-picker.stories.tsx
@@ -1,6 +1,6 @@
 import type {Meta, StoryObj} from "@storybook/react";
 import * as React from "react";
-import {DatePicker} from "../../ui/form/date-picker";
+import {DatePicker} from "src/ui/form/date-picker";
 
 type DateFieldDemoProps = {
     mode?: "year-month" | "year-month-day";
